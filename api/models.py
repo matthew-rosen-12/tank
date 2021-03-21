32fb18d9121fd1bcba2878196019c59e47036162
Matthew Rosen <mhr79@cornell.edu>
2021-03-21 13:06:06 -0400

    Models, User is abstract user

---
+++ api/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+# class Client(models.Model):
+# 	user = models.OneToOneField(User, null=True,on_delete=models.cascade)
+#     name = models.CharField(max_length=100)
+#     email = models.CharField(max_length=120)
+
+#     def __str__(self):
+#         return self.name
+
+#     class Meta:
+#         ordering = ('name',)
+
+
+class Tank(models.Model):
+    title = models.CharField(max_length=120)
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
+class Fish(models.Model):
+    breed = models.CharField(max_length=50)
+    speed = models.SmallIntegerField()
+    tank = models.ForeignKey(Tank, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.breed
+
+    class Meta:
+        ordering = ('tank',)
+
+    def save(self, *args, **kwargs):
+        validBreeds = ['sun']
+        if self.breed in validBreeds:
+            super(Foo, self).save(*args, **kwargs)
+        else:
+            raise Exception("Invalid Breed")
